8d096337f0bea52bffbaecfc09112177327967d7
rodrigoemcode <rodrigosilva.emcode@gmail.com>
2024-06-21 09:14:16 -0300

    Generates a random password based on the specified parameters.

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,30 @@
 import random
 import string
 
+
 def generate_random_password(length, use_upper=True, use_lower=True, use_digits=True, use_symbols=True):
     """
     Generates a random password based on the specified parameters.
 
+    :param length: Length of the password.
+    :param use_upper: Include uppercase letters.
+    :param use_lower: Include lowercase letters.
+    :param use_digits: Include digits.
+    :param use_symbols: Include symbols.
+    :return: Generated password.
+    """
+    if length <= 0:
+        raise ValueError("Password length must be greater than zero.")
+
+    all_characters = ''
+    if use_upper:
+        all_characters += string.ascii_uppercase
+    if use_lower:
+        all_characters += string.ascii_lowercase
+    if use_digits:
+        all_characters += string.digits
+    if use_symbols:
+        all_characters += string.punctuation
+
+    if not all_characters:
+        raise ValueError("At least one type of character must be selected.")
