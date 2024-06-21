8e28cda6729edee243f1442a360a552a09e372ad
rodrigoemcode <rodrigosilva.emcode@gmail.com>
2024-06-21 08:51:23 -0300

    Adding "if"

--- password_generator.py
+++ password_generator.py
@@ -4,3 +4,16 @@ uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 lowercase_letters = uppercase_letters.lower()
 digits = '0123456789'
 symbols = '@#$%¨&*()_+`{^}?:><'
+
+upper, lower, nums, syms = True, True, True, True
+
+all = ''
+
+if upper:
+    all += uppercase_letters
+if lower:
+    all += lowercase_letters
+if nums:
+    all += digits
+if syms:
+    all += symbols
